3c34cda6e8342d8dd720201b0d864ddd08e401bb
Max Provin <maxprovin@outlook.com>
2016-04-13 13:09:28 +0100

    refactor spec step13 spec test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -28,11 +28,9 @@ describe DockingStation do
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
 
-    it 'raises an error when docking a bike when a bike is already docked' do
-      bike = Bike.new
-      bike2 = Bike.new
-      subject.dock(bike)
-      expect { subject.dock(bike2) }.to raise_error 'Bike is already docked!'
+    it 'raises an error when docking a bike if a bike is already docked' do
+      subject.dock(Bike.new)
+      expect { subject.dock(Bike.new) }.to raise_error 'Bike is already docked!'
     end
 
   end
