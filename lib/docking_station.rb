e5f0ded34b238c9b4da3922b89df04fb6a8b9b2f
hnamitha1 <namitha.h@gmail.com>
2016-04-13 15:24:28 +0100

    challenge 15 :single Responsibility Principle

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,13 +7,23 @@ end
 attr_reader :bikes
 
   def release_bike
-  	fail 'No bikes available' if @bikes.count <= 0
+  	fail 'No bikes available' if empty?
     @bikes.pop()
   end
 
   def dock(bike)
-    fail 'Bike is already docked!' if @bikes.count >= 20
+    fail 'Bike is already docked!' if full?
     @bikes.push(bike) 
   end
+
+  private
+
+   def full?
+      @bikes.count >= 20 
+   end	
+
+   def empty?
+   	   @bikes.count <= 0
+   end	   
 
 end
